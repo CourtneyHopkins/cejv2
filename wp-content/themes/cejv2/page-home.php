c83645665a06534d3804fb7e935c3ece93444924
Courtneyjones <me@courtneyejones.com>
2014-04-14 09:27:35 -0400

    page home

---
+++ wp-content/themes/cejv2/page-home.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: Homepage
+*/
+?>
+
+<?php get_header(); ?>
+
+			<div id="content">
+				<div id="inner-content" class="clearfix">
+					<div id="hello">
+						<span class="salutation">hey there!</span><br /> 
+						<br /><br />
+						<span class="intro">I'm <span class="green">Courtney Jones</span>,<br />
+						a web developer &amp; interface designer.</span>
+						<br />
+						<span class="intro green">But that's not all I do... </span>
+						<br /><br /><br />
+						<div class="buttons clearfix">
+							<a href="/about/"><div class="fourcol first">Learn more about me</div></a>
+							<a href="/portfolio/"><div class="fourcol">View my work</div></a>
+							<a href="/about/"><div class="fourcol last">Contact me</div></a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+<?php get_footer(); ?>
