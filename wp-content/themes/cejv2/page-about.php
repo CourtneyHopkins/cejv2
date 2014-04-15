e9399599365eecb1b9d959492d590be985aa62dd
Courtneyjones <me@courtneyejones.com>
2014-04-15 00:36:59 -0400

    About page design and development

--- wp-content/themes/cejv2/page-about.php
+++ wp-content/themes/cejv2/page-about.php
@@ -7,8 +7,86 @@ Template Name: About
 <?php get_header(); ?>
 
             <div id="content">
-                <div id="inner-content" class="resume clearfix">
-                    <p>My experience in web design and development so far has enlightened me to the frustration between Web Designers and Web Developers. Knowing how both worlds work is ESSENTIAL in streamlining the efficiency of a project and enhancing team synergy. Being able to communicate &amp; perform both design and programming means less confusion between expectation of the client and execution of the final product. It also means less time spent re-working designs that don’t meet realistic programming needs. The product needs to function seamlessly so that clients BENEFIT from its qualities without frustration. I believe that with my perspective, skills, creativity and efficiency, I can help make that happen.</p>
+                <div id="inner-content" class="clearfix">
+                    <div id="hello" class="clearfix">
+                        <div class="fivecol first">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/me.png" alt="Courntey E. Jones Web Development and Design" />
+                        </div>    
+                        <div class="sevencol last">
+                            <span class="intro">My prime directive is to be the kryptonite to bad design and poor user experiences.</span>
+                            <br /><br />
+                            <div class="buttons">
+                                <a href="">
+                                    <div>See what I'm up to</div>
+                                </a>
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="main-description clearfix">
+                        <p>My experience in <span class="green">web design</span> and <span class="green">development</span> so far has enlightened me to the <span class="green">frustration between Web Designers and Web Developers</span>. Knowing how <span class="green">both</span> worlds work is <span class="green">ESSENTIAL</span> in streamlining the <span class="green">efficiency</span> of a project and <span class="green">enhancing</span> team synergy. Being able to <span class="green">communicate &amp; perform</span> both design and programming means <span class="green">less confusion</span> between expectation of the client and <span class="green">execution</span> of the final product. It also means <span class="green">less time spent re-working designs</span> that don’t meet realistic programming needs. The product needs to <span class="green">function seamlessly</span> so that clients <span class="green">BENEFIT</span> from its qualities without frustration. I believe that with my perspective, skills, creativity and efficiency, <span class="green">I can help make that happen.</span></p>
+                    </div>
+                    <div class="skill-wrapper">
+                        <div class="clearfix">
+                            <div class="fourcol first">
+                                <div class="skill">Code</div>
+                                <ul>
+                                    <li>HTML / CSS / SCSS</li>
+                                    <li>jQuery</li>
+                                    <li>PHP</li>
+                                    <li>Wordpress Theme Development</li>
+                                    <li>Ruby</li>
+                                    <li>Responsive Development</li>
+                                    <li>Github</li>
+                                </ul>
+                                <br />
+                                <div class="buttons">
+                                    <a href="">
+                                        <div>See related work</div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="fourcol">
+                                <div class="skill">Design</div>
+                                <ul>
+                                    <li>UI / UX Design</li>
+                                    <li>Photoshop</li>
+                                    <li>Illustrator</li>
+                                    <li>InDesign</li>
+                                    <li>Web / Mobile</li>
+                                    <li>Print</li>
+                                    <li>Pen &amp; Ink</li>
+                                    <li>Watercolor</li>
+                                </ul>
+                                <div class="buttons">
+                                    <a href="">
+                                        <div>See related work</div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="fourcol last">
+                                <div class="skill">Strategy</div>
+                                <ul>
+                                    <li>Branding</li>
+                                    <li>Responsive Design</li>
+                                    <li>Mobile-First Approach</li>
+                                    <li>Concept Design</li>
+                                    <li>Creative Visualization</li>
+                                    <li>User Interaction</li>
+                                </ul>
+                                <br />
+                                <br />
+                                <div class="buttons">
+                                    <a href="">
+                                        <div>See related work</div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <br />
+                        <br />
+                        <br />
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/skillz.png" alt="Courtney E. Jones's Development and Design Skills" />
+                    </div>
                 </div>
             </div>
 
